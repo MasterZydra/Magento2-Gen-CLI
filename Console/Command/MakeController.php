e94f2c48f8bbdb067066fb145c2e7f3150ba4f72
MasterZydra <github@davidhein.de>
2024-11-01 21:10:30 +0100

    Improved command `make:module` and added command `make:controller`

--- Console/Command/MakeController.php
+++ Console/Command/MakeController.php
@@ -12,10 +12,12 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class MakeModule extends Command
+class MakeController extends Command
 {
     private const VENDOR = 'vendor';
     private const MODULE = 'module';
+    private const SECTION = 'section';
+    private const ACTION = 'action';
 
     /** @inheritdoc */
     public function __construct(
@@ -28,10 +30,12 @@ class MakeModule extends Command
     /** @inheritdoc */
     protected function configure(): void
     {
-        $this->setName('make:module');
-        $this->setDescription('Create a new module');
+        $this->setName('make:controller');
+        $this->setDescription('Create a new controller');
         $this->addArgument(self::VENDOR, InputArgument::OPTIONAL, 'Vendor name (e.g. \'Magento\')');
         $this->addArgument(self::MODULE, InputArgument::OPTIONAL, 'Module name (e.g. \'Sales\')');
+        $this->addArgument(self::SECTION, InputArgument::OPTIONAL, 'Section name (e.g. \'Index\')');
+        $this->addArgument(self::ACTION, InputArgument::OPTIONAL, 'Action name (e.g. \'Index\')');
         parent::configure();
     }
 
@@ -57,47 +61,62 @@ class MakeModule extends Command
             }
         }
 
+        $modulePath = $this->helper->joinPaths($this->helper->appCodePath(), $vendor, $module);
+
+        // Check if module exists
+        if (!$this->helper->exists($modulePath)) {
+            $output->writeln("Module '$vendor/$module' does not exist!");
+            return 1;
+        }
+
+        // User inputs
+        $section = $input->getArgument(self::SECTION);
+        if (empty($section)) {
+            $section = $this->helper->askQuestion($input, $output, 'Section name (e.g. \'Index\'): ', null);
+            if (empty($section)) {
+                $output->writeln('Section name is required!');
+                return 1;
+            }
+        }
+
+        $action = $input->getArgument(self::ACTION);
+        if (empty($action)) {
+            $action = $this->helper->askQuestion($input, $output, 'Action name (e.g. \'Index\'): ', null);
+            if (empty($action)) {
+                $output->writeln('Action name is required!');
+                return 1;
+            }
+        }
+
         $output->writeln('');
 
-        $modulePath = $this->helper->joinPaths($this->helper->appCodePath(), $vendor, $module);
-        $moduleName = $vendor. '_' . $module;
+        $controllerPath = $this->helper->joinPaths($modulePath, 'Controller', $section, $action . '.php');
 
-        // Create module folder
-        if ($this->helper->exists($modulePath)) {
-            $output->writeln("Module '$vendor/$module' already exists!");
+        // Check if controller already exists
+        if ($this->helper->exists($controllerPath)) {
+            $output->writeln("Controller '$section/$action' already exists!");
             return 1;
         }
 
-        if (!$this->helper->mkDir($modulePath)) {
-            $output->writeln('An error occured while creating the module directory!');
-            return 1;
-        }
+        $output->writeln('Generating controller...');
 
-        $output->writeln('Generating module...');
-
-        // Generate files
-        // Source: https://experienceleague.adobe.com/en/docs/commerce-learn/tutorials/backend-development/create-module
-
+        // Generate file
         if (!$this->helper->copyTemplate(
-            $this->helper->joinPaths($this->helper->templatePath(), 'registration.php.txt'),
-            $this->helper->joinPaths($modulePath, 'registration.php'),
-            ['{{ modulename }}' => $moduleName],
-        )) {
-            $output->writeln('An error occured while creating \'registration.php\'');
-            return 1;
-        }
-
-        if (!$this->helper->copyTemplate(
-            $this->helper->joinPaths($this->helper->templatePath(), 'etc', 'module.xml'),
-            $this->helper->joinPaths($modulePath, 'etc', 'module.xml'),
-            ['{{ modulename }}' => $moduleName],
+            $this->helper->joinPaths($this->helper->templatePath(), 'Controller', 'Section', 'Action.php.txt'),
+            $controllerPath,
+            [
+                '{{ vendor }}' => $vendor,
+                '{{ module }}' => $module,
+                '{{ section}}' => $section,
+                '{{ action }}' => $action,
+            ],
         )) {
             $output->writeln('An error occured while creating \'ect/module.xml\'');
             return 1;
         }
 
         $output->writeln('');
-        $output->writeln("Module '$vendor/$module' was created.");
+        $output->writeln("Controller '$section/$action' was created.");
         return 0;
     }
 }
---
+++ README.md
@@ -0,0 +1,42 @@
+# Magento2 Generator CLI
+
+**Content**  
+- [Commands](#commands)
+- [Usage](#usage)
+- [Installation](#installation)
+- [Updating to latest version](#updating-to-latest-version)
+
+Creating a new module for Magento2, adding a controller, etc. is not intuitive.  
+You have to look into the official documentation of Magento2 or use a third party tool like [Mage2Gen.com](Mage2Gen.com).  
+This extension adds Laravel-like CLI commands to simplify these steps.
+
+## Commands
+| Command | Description |
+|---------|-------------|
+| `make:controller` | Create a new controller |
+| `make:module` | Create a new module |
+
+## Usage
+```
+> php bin/magento make:module <vendor name> <module name>
+
+> php bin/magento make:module
+Vendor name (e.g. 'Magento'): Magento
+Module name (e.g. 'Sales'): Sales
+Generating files...
+Module 'Magento/Sales' was created.
+```
+
+## Installation
+This Magento2 module can be installed using composer:  
+`> composer require masterzydra/masterzydra/magento2-gen-cli`
+
+To remove it from the list of required packages use the following command:  
+`> composer remove masterzydra/masterzydra/magento2-gen-cli`
+
+## Updating to latest version
+With the following command composer checks all packages in the composer.json for the latest version:  
+`> composer update`
+
+If you only want to check this package for newer versions, you can use  
+`> composer update masterzydra/masterzydra/magento2-gen-cli`
